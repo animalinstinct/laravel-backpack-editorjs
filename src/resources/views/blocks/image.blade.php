0a5409589d4bcf2032818f40ade71fdf999c8dfa
Aleksandr Makarov <makarov@owlcat.games>
2024-12-26 16:02:35 +0700

    Images inline styling according to type

--- src/resources/views/blocks/image.blade.php
+++ src/resources/views/blocks/image.blade.php
@@ -1,18 +1,23 @@
 @php
 $classes = '';
-if ($data['stretched']){
-$classes .= ' image--stretched';
+$styles = '';
+
+if ($data['stretched']) {
+    $classes .= ' image--stretched';
+    $styles .= 'width: 100%;height: auto;';
 }
-if ($data['withBorder']){
-$classes .= ' image--bordered';
+if ($data['withBorder']) {
+    $classes .= 'image--bordered';
+    $styles .= 'border: 7px solid #393939;';
 }
-if ($data['withBackground']){
-$classes .= ' image--backgrounded';
+if ($data['withBackground']) {
+    $classes .= ' image--backgrounded';
+    $styles .= 'margin: 0 auto;padding: 1em 0;';
 }
 @endphp
 
 <figure class="image {{ $classes }}">
-    <img src="{{ $data['file']['url'] }}" alt="{{ $data['caption'] ?: '' }}">
+    <img src="{{ $data['file']['url'] }}" alt="{{ $data['caption'] ?: '' }}" style="{{ $styles }}">
     @if (!empty($data['caption']))
     <footer class="image-caption">
         {{ $data['caption'] }}
